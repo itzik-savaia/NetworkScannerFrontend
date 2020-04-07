41b44e7a189e2fc255f046dd1b8a40ca57bfe8a5
itzik savaia <44684932+itzikking@users.noreply.github.com>
2020-04-07 23:37:58 +0300

    bla

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/network-scanner-frontend'));
+app.use(express.static(__dirname + '/dist/NetworkScannerFrontend'));
 
 app.get('/*', function (req, res) {
-    const fullPath = path.join(__dirname + '/dist/network-scanner-frontend/index.html');
+    const fullPath = path.join(__dirname + '/dist/NetworkScannerFrontend/index.html');
     console.log(" Fetching from.." + fullPath);
     res.sendFile(fullPath);
 })
