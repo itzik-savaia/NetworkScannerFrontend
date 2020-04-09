1f019460f2ddcb27865d711ec0f9530d65f80147
itzik savaia <44684932+itzikking@users.noreply.github.com>
2020-04-09 10:38:06 +0300

    b

--- server.js
+++ server.js
@@ -9,13 +9,16 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/NetworkScannerFrontend'));
-app.use(cors())
 
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', 'https://networkscanner-s.herokuapp.com/');
-//     next();
-// });
-app.options('https://networkscanner-s.herokuapp.com', cors())
+var corsOptions = {
+    origin: 'http://example.com',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.get(cors(corsOptions), function (req, res, next) {
+    res.json({ msg: 'This is CORS-enabled for only example.com.' })
+})
+
 
 
 app.get('/*', function (req, res) {
