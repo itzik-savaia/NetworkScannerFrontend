84c5f80a69ba4dc2214963524834c3a03c9f27d2
itzik savaia <44684932+itzikking@users.noreply.github.com>
2020-04-09 15:28:10 +0300

    b

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ConnectionService } from './services/connection.service'
+import { HubConnection } from '@microsoft/signalr';
+import * as signalR from '@microsoft/signalr';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -7,11 +11,72 @@ import { ConnectionService } from './services/connection.service'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  private _hubConnection: HubConnection | undefined;
+  hubConnection;
+  SendMessage: FormGroup;
+  messages: any[] = [];
+  message: any[] = [];
+  start_ip
+  end_ip
+  ErrorMsg
 
-  constructor(public ConnectionService: ConnectionService, ) { }
+  data200 = [];
+  data400 = [];
+  constructor(private _formBuilder: FormBuilder) {
+    this.hubConnection = this._hubConnection
+
+    this.hubConnection = new signalR.HubConnectionBuilder()
+      .withUrl('https://networkscanner-s.herokuapp.com/ip')
+      .configureLogging(signalR.LogLevel.Information)
+      .build();
+
+    this.hubConnection.start().catch(err => console.log(err.toString()));
+    this.hubConnection.on('Send', (data: any) => {
+      this.data200.push(data.endPoint);
+      if (data.status == 200) {
+        console.log(data);
+        this.data200.push(data.endPoint);
+        this.messages.push(data.endPoint);
+      } else {
+        this.ErrorMsg = data
+        console.log(this.ErrorMsg);
+        setTimeout(() => {
+          this.ErrorMsg = undefined
+        }, 5000);
+        this.data400.push(data.endPoint);
+      }
+    });
+  }
 
   ngOnInit() {
-    this.ConnectionService.fromValid();
-    this.ConnectionService.connection();
+    this.SendMessage = this._formBuilder.group({
+      StartIP: [null,
+        [
+          Validators.required,
+          Validators.pattern("^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$")
+        ]],
+      EndIP: [null,
+        [
+          Validators.required,
+          Validators.pattern("^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$")
+        ]],
+    })
+  }
+  IpRange(): void {
+    this.start_ip = this.SendMessage.value.StartIP;
+    this.end_ip = this.SendMessage.value.EndIP;
+  }
+  public sendMessage(): void {
+    this.start_ip = this.SendMessage.value.StartIP;
+    this.end_ip = this.SendMessage.value.EndIP;
+    const data = this.start_ip + ',' + this.end_ip;
+    try {
+      if (this.hubConnection) {
+        this.hubConnection.invoke('Send', data);
+      }
+      this.messages.push(data);
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
