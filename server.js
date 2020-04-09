3509fac3934ed50222dae7908c269d7fc41387ec
itzik savaia <44684932+itzikking@users.noreply.github.com>
2020-04-09 10:42:43 +0300

    b

--- server.js
+++ server.js
@@ -1,25 +1,21 @@
 
 
 
-//Install express server
 const express = require('express');
 const path = require('path');
 const cors = require('cors')
 const app = express();
 
-// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/NetworkScannerFrontend'));
 
 var corsOptions = {
     origin: 'https://networkscanner-s.herokuapp.com',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    optionsSuccessStatus: 200
 }
 
 app.get(cors(corsOptions), function (req, res, next) {
     res.json({ msg: 'This is CORS-enabled for only example.com.' })
 })
-
-
 
 app.get('/*', function (req, res) {
     const fullPath = path.join(__dirname + '/dist/NetworkScannerFrontend/index.html');
@@ -27,5 +23,4 @@ app.get('/*', function (req, res) {
     res.sendFile(fullPath);
 })
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
